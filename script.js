88fe77c291596b524a2bc4b9e62ac93e9e40dc03
pritamsahu99 <pritamsahuofficial@gmail.com>
2023-12-22 20:40:13 +0530

    update

--- script.js
+++ script.js
@@ -19,13 +19,14 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
-  async function recursiveSearch(remainingSearches) {
+  function recursiveSearch(remainingSearches) {
     if (remainingSearches > 0) {
-      generateRandomWord(async function (randomWord) {
+      generateRandomWord(function (randomWord) {
         if (randomWord) {
           usedWords.push(randomWord);
-          await searchGoogle(randomWord);
-          recursiveSearch(remainingSearches - 1);
+          searchGoogle(randomWord, function () {
+            recursiveSearch(remainingSearches - 1);
+          });
         } else {
           usedWords = [];
           alert('All words have been used. Resetting the list.');
@@ -34,24 +35,21 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  async function generateRandomWord(callback) {
-    const apiUrl = 'https://random-word-api.herokuapp.com/all';
-    try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      const randomWord = data[Math.floor(Math.random() * data.length)];
-      callback(randomWord);
-    } catch (error) {
-      console.error('Error fetching random word:', error);
-      callback(null);
+  function generateRandomWord(callback) {
+    const meaningfulCombinations = ['sky', 'down', 'science', 'random', 'coding', 'javascript', 'gpt', 'extension', 'meaningful'];
+    const availableWords = meaningfulCombinations.filter(word => !usedWords.includes(word));
+
+    if (availableWords.length === 0) {
+      return null;
     }
+
+    const randomIndex = Math.floor(Math.random() * availableWords.length);
+    callback(availableWords[randomIndex]);
   }
 
-  async function searchGoogle(query) {
-    return new Promise(resolve => {
-      chrome.tabs.update(currentTabId, { url: `https://www.google.com/search?q=${encodeURIComponent(query)}` }, function () {
-        setTimeout(resolve, 4000);
-      });
+  function searchGoogle(query, callback) {
+    chrome.tabs.update(currentTabId, { url: `https://www.google.com/search?q=${encodeURIComponent(query)}` }, function () {
+      setTimeout(callback, 2000);
     });
   }
 });
