43ec2090034c973462f53b8806ff179490c794d2
pritamsahu99 <pritamsahuofficial@gmail.com>
2023-12-22 20:03:37 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function () {
   const randomWordElement = document.getElementById('random-word');
   let usedWords = [];
@@ -29,7 +28,6 @@ document.addEventListener('DOMContentLoaded', function () {
             recursiveSearch(remainingSearches - 1);
           });
         } else {
-          // All words have been used, reset the list
           usedWords = [];
           alert('All words have been used. Resetting the list.');
         }
@@ -38,54 +36,19 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function generateRandomWord(callback) {
-    const randomWord = getRandomWord();
-
-    if (randomWord) {
-      callback(randomWord);
-    } else {
-      // All words have been used, reset the list
-      usedWords = [];
-      alert('All words have been used. Resetting the list.');
-    }
-  }
-
-  function getRandomWord() {
-    // const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    const meaningfulCombinations = [
-      'sky', 'down', 'science', 'random', 'coding', 'javascript', 'gpt', 'extension', 'meaningful'
-      // Add more meaningful combinations as needed
-    ];
+    const meaningfulCombinations = ['sky', 'down', 'science', 'random', 'coding', 'javascript', 'gpt', 'extension', 'meaningful'];
     const availableWords = meaningfulCombinations.filter(word => !usedWords.includes(word));
 
-    // const availableWords = generateCombinations(alphabet.split(''), 3) 
-    //                         .filter(word => !usedWords.includes(word));
-
     if (availableWords.length === 0) {
-      return null; // Signal that all words have been used
+      return null;
     }
 
     const randomIndex = Math.floor(Math.random() * availableWords.length);
-    return availableWords[randomIndex];
-  }
-
-  function generateCombinations(letters, length) {
-    const combinations = [];
-    const generate = (prefix, remainingLength) => {
-      if (remainingLength === 0) {
-        combinations.push(prefix.join(''));
-        return;
-      }
-      for (const letter of letters) {
-        generate(prefix.concat(letter), remainingLength - 1);
-      }
-    };
-    generate([], length);
-    return combinations;
+    callback(availableWords[randomIndex]);
   }
 
   function searchGoogle(query, callback) {
     chrome.tabs.update(currentTabId, { url: `https://www.google.com/search?q=${encodeURIComponent(query)}` }, function () {
-      // Wait for a short time to ensure the page is loaded before triggering the next search
       setTimeout(callback, 4000);
     });
   }
